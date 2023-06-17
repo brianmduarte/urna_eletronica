64222d3a23520e1731d2dda54d35642834a10598
Brian Duarte <braianmucioduarte@gmail.com>
2023-06-17 00:28:01 -0300

    Direcionando projeto para o Github

---
+++ README.md
@@ -0,0 +1,25 @@
+<h3>Projeto - Urna Eletrônica</h3>
+<br>
+
+Este projeto teve como intuito desenvolver uma interface que simula a urna eletrônica brasileira e os seus recursos
+para a escolha de um candidato. 
+
+No projeto foi utilizado uma base para "criar" o candidatos e outra para desenvolver
+as regras e dinâmica do voto.
+
+Para ver a animação, vote em um dos vereadores abaixo:
+1º opção de Vereador - 38111
+2º opção de Vereador - 77222
+
+*Se quiser avançar para o voto em Prefeito sem escolher um candidato a Vereador, basta votar em Branco.
+<br>
+
+Para ver a animação para voto em Prefeito:
+1º opção de Vereador - 10
+2º opção de Vereador - 15
+
+Foi um desafio muito divertido e pude aprimorar ainda mais as habilidades com o Javascript e conhecer na prática a amplitude de seus recursos e funções. Espero que gostem!
+
+<br>
+
+link do projeto: 
---
+++ urna/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+            titulo: 'VEREADOR',
+            numeros: 5,
+            candidatos: [
+                {
+                    numero: '38111',
+                    name: 'José Silva',
+                    partido: 'Coxinhas',
+                    fotos: [
+                        {url:'5556529.png', legenda: 'Vereador'}
+                    ]
+                },
+
+                {
+                    numero: '77222',
+                    name: 'Brian Duarte',
+                    partido: 'Desenvolvedores Front-end',
+                    fotos: [
+                        {url: '5556468.png', legenda: 'Vereador'}
+                    ]
+                },
+            ]
+    },
+    
+    {
+            titulo: 'PREFEITO',
+            numeros: 2,
+            candidatos: [
+                {
+                    numero: '10',
+                    name: 'Brendinha',
+                    partido: 'Bancários Anônimos',
+                    vice: 'Nelzinha',
+                    fotos: [
+                        {url:'iconfinder-4-avatar-2754580_120522.png', legenda: 'Prefeito'},
+                        {url: '10594872.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+                {
+                    numero: '15',
+                    name: 'Zezinho da Padaria',
+                    partido: 'Sociedade dos pães',
+                    vice: 'Cleitin',
+                    fotos: [
+                        {url:'avatar-de-personne-icone-homme.png', legenda: 'Prefeito'},
+                        {url:'5556529.png', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                },
+            ]
+
+    }
+];
